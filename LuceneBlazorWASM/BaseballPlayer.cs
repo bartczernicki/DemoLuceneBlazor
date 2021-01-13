7f698f76c07c9d2357f75740b4522637925f4ece
Bart Czernicki <bartczernicki@outlook.com>
2021-01-13 11:56:13 -0500

    Added Blazor WASM project for UI for Lucene index.

---
+++ LuceneBlazorWASM/BaseballPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuceneBlazorWASM
+{
+    public class BaseballPlayer
+    {
+        public string FullPlayerName { get; set; }
+
+        public int YearsPlayed { get; set; }
+
+        public int Hits { get; set; }
+    }
+}
